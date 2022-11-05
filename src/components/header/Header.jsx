27b26804b15c86554e090145a581491c947fb223
mnwachukwu042 <mnwachukwu042@gmail.com>
2022-11-05 17:39:42 +0100

    next commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+// import Heropic from "../../assets/images/everst.jpg";
+import Heropic from "../../assets/images/1a501ey1uwe91.jpg";
+
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm glow">...coming soon</span>
+        {/* <span className="headerTitleLg">Blog</span> */}
+      </div>
+      <img className="headerImg" src={Heropic} alt="" />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,55 @@
+import "./sidebar.css";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import ProfilePic from "../../assets/images/posters-composition-with-3d-cubes.jpg-removebg-preview.png";
+
+const Sidebar = () => {
+  const [cats, setCats] = useState([]);
+  const getCats = async () => {
+    const res = await axios.get("/categories");
+    setCats(res.data);
+  };
+
+  useEffect(() => {
+    getCats();
+  }, []);
+
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT US</span>
+        {/* {post.photo && <img src={post.photo} alt="" />} */}
+        <img src={ProfilePic} alt="" />
+        <p>
+          Marvel Bloggers we are passionate about Marvel Movies He is a hard
+          worker and a rational thinker who loves to logically deconstruct a
+          problem to find innovative solutions. With a multi disciplinary
+          approach in life, he always gives emphasis on being a team player and
+          recognises how reliability can lead to success.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          {cats.map((c, i) => (
+            <Link key={i} to={`/?cat=${c.name}`} className="link">
+              <li className="sidebarListItem">{c.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+          <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+          <i className="sidebarIcon fa-brands fa-linkedin"></i>
+          <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
